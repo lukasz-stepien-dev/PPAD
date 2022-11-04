6e9845c6fa6e2da5793bec8055257793200e1827
Łukasz Stępień <lukaszatepien@gmail.com>
2022-11-04 13:45:02 +0100

    Example 1

---
+++ 04.11.22-Classes/04.11.22-Classes/04.11.22-Classes.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Punkt
+{
+private:
+    int x;
+    int y;
+
+public:
+    void setX(int x)
+    {
+        this->x = x;
+    }
+
+    int getX()
+    {
+        return this->x;
+    }
+
+    void setY(int y)
+    {
+        this->y = y;
+    }
+
+    int getY()
+    {
+        return this->y;
+    }
+};
+
+int main() {
+    Punkt p1;
+
+    p1.setX(2);
+    p1.setY(5);
+
+    cout << "( " << p1.getX() << ", " << p1.getY() << " )";
+
+    return 0;
+}
