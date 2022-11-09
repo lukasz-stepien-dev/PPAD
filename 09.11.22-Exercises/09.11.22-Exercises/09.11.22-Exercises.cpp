309f317005e2ae5ddaece7100e661b04ad68cb12
Łukasz Stępień <lukaszatepien@gmail.com>
2022-11-09 09:21:35 +0100

    Added ex1

--- 09.11.22-Exercises/09.11.22-Exercises/09.11.22-Exercises.cpp
+++ 09.11.22-Exercises/09.11.22-Exercises/09.11.22-Exercises.cpp
@@ -1,7 +1,27 @@
 ﻿#include <iostream>
+#include <cmath>
+
+// Łukasz Stępień
+
+using namespace std;
+
+
+
+//  
+float ex1(int brutto, float tax)
+{
+    float taxPct = 1;
+    taxPct -= tax / 100;
+    return brutto * taxPct;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int brutto = 0, tax = 0;
+    cout << "Podaj kwote brutto: ";
+    cin >> brutto;
+    cout << "Podaj podatek: ";
+    cin >> tax;
+    cout << "Cena netto: " << ex1(brutto, tax);
 }
 
