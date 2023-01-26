a71cceedcf86dba64ab62eaec79028bc10599238
Łukasz Stępień <lukaszatepien@gmail.com>
2023-01-26 12:56:00 +0100

    Add Hello World and Intructions for ex.2

---
+++ 04.01.23 Assigments/Ex2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Instructions
+
+Utwórz zmienna strukturalną, wypełnij ja wartościami zdefiniowanymi dla tej struktury i wyświetl je na ekranie.
+
+A. Napisz funkcję, która przyjmuje wartość struktury pudelko i wyświetla każdą składową.
+B. Napisz funkcję, która przyjmuje adres struktury pudelko, oblicza i ustawia składowe pole i objetosc
+C. Napisz funkcję, która przyjmuje referencję do struktury pudelko i wyświetla każdą składową. W jaki sposób zabezpieczyć strukturę przed ewentualnymi zmianami wartości składowych dokonanymi przez użytkownika
+D. Napisz funkcję, która przyjmuje referencję do struktury pudelko, oblicza i ustawia składowe pole i objetosc.
+*/
+
+int main()
+{
+
+    return 0;
+}
