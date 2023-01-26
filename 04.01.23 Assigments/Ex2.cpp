6afb191e41c7b65e9b709b776df5e4ebfb27c037
Łukasz Stępień <lukaszatepien@gmail.com>
2023-01-26 17:21:38 +0100

    Remove unnecessary intructions

--- 04.01.23 Assigments/Ex2.cpp
+++ 04.01.23 Assigments/Ex2.cpp
@@ -9,9 +9,7 @@ Instructions
 Utwórz zmienna strukturalną, wypełnij ja wartościami zdefiniowanymi dla tej struktury i wyświetl je na ekranie.
 
 A. Napisz funkcję, która przyjmuje wartość struktury pudelko i wyświetla każdą składową.
-B. Napisz funkcję, która przyjmuje adres struktury pudelko, oblicza i ustawia składowe pole i objetosc
-C. Napisz funkcję, która przyjmuje referencję do struktury pudelko i wyświetla każdą składową. W jaki sposób zabezpieczyć strukturę przed ewentualnymi zmianami wartości składowych dokonanymi przez użytkownika
-D. Napisz funkcję, która przyjmuje referencję do struktury pudelko, oblicza i ustawia składowe pole i objetosc.
+B. Napisz funkcję, która przyjmuje referencję do struktury pudelko, oblicza i ustawia składowe pole i objetosc.
 */
 
 #define Console(x) cout << x << endl;
