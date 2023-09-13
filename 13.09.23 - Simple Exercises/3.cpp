2f31083d3bff5a91eb3e1168c9e2f038f82d0bf7
Łukasz Stępień <93351383+lukasz-stepien-dev@users.noreply.github.com>
2023-09-13 08:43:09 +0200

    Add 3.cpp

---
+++ 13.09.23 - Simple Exercises/3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i < 51; i++) {
+        if (i % 2 == 0) {
+            cout << i << endl;
+        }
+    }
+    return 0;
+}
